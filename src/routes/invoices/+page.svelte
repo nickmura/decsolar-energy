313f273c8bdfd778a118aaa51985ab95da02056b
patrickbdevaney <79435941+patrickbdevaney@users.noreply.github.com>
2023-03-04 19:12:57 -0700

    removing sidebar

--- src/routes/invoices/+page.svelte
+++ src/routes/invoices/+page.svelte
@@ -6,4 +6,52 @@
 </script>
 
 
+<div>
+    <div class="absolute" style="width: 876px; height: 702px; left: 402px; top: 299px;">
+        <div class="inline-flex flex-col space-y-7 items-start justify-start pb-72 bg-white shadow rounded-2xl" style="width: 876px; height: 702px;">
+            <img class="rounded-2xl" style="width: 876px; height: 203px;" src="../../../../img/solarBaranquilla.jpg" alt="Solar Baranquilla"/>
+            <div class="flex flex-col items-start justify-start" style="width: 783px; height: 166.65px;">
+                <p class="text-2xl font-bold text-gray-800" style="width: 424.91px; height: 62.65px;">Hospital Of Baranquilla</p>
+                <p class="w-72 h-12 text-lg text-gray-500">Baranquilla, Colombia</p>
+                <p class="text-2xl font-bold text-gray-800" style="width: 424.91px; height: 62.65px;">Total Usage Month Kwh</p>
+            </div>
+        </div>
+    </div>
+    <div class="absolute" style="width: 876px; height: 702px; left: 402px; top: 299px;">
+        <div class="inline-flex flex-col space-y-7 items-start justify-start pb-72 bg-white shadow rounded-2xl" style="width: 876px; height: 702px;">
+            <img class="rounded-2xl" style="width: 876px; height: 203px;" src="https://via.placeholder.com/876x203"/>
+            <div class="flex flex-col items-start justify-start" style="width: 783px; height: 166.65px;">
+                <p class="text-2xl font-bold text-gray-800" style="width: 424.91px; height: 62.65px;">Hospital Of Baranquilla</p>
+                <p class="w-72 h-12 text-lg text-gray-500">Baranquilla, Colombia</p>
+                <p class="text-2xl font-bold text-gray-800" style="width: 424.91px; height: 62.65px;">Total Usage Month Kwh</p>
+            </div>
+        </div>
+    </div>
+    <div class="absolute" style="width: 876px; height: 702px; left: 402px; top: 299px;">
+        <div class="flex items-end justify-start pl-6 pr-16 pt-56 pb-36 bg-white shadow rounded-2xl" style="width: 876px; height: 702px;">
+            <div class="relative" style="width: 783px; height: 322.65px;">
+                <p class="absolute right-0 bottom-0 text-2xl font-bold text-gray-800" style="width: 424.91px; height: 62.65px;">Hospital Of Baranquilla</p>
+                <p class="absolute right-0 bottom-0 text-2xl font-bold text-gray-800" style="width: 424.91px; height: 62.65px;">Total Usage  Kwh</p>
+                <p class="w-72 h-12 absolute right-0 bottom-0 text-lg text-gray-500">Baranquilla, Colombia</p>
+                <p class="w-72 h-12 absolute right-0 bottom-0 text-2xl text-gray-500">20,000 Kwh</p>
+                <p class="absolute right-0 bottom-0 text-2xl font-bold text-gray-800" style="width: 424.91px; height: 62.65px;">Cost per 1 Kwh</p>
+                <p class="w-72 h-12 absolute right-0 bottom-0 text-2xl text-gray-500">$ .17 cents/Kwh</p>
+                <p class="absolute right-0 bottom-0 text-2xl font-bold text-gray-800" style="width: 424.91px; height: 62.65px;">Total Cost Electricity Bill </p>
+                <p class="w-72 h-12 absolute right-0 bottom-0 text-2xl text-gray-500">$ 3400 USD</p>
+            </div>
+        </div>
+    </div>
+    <button class="w-56 h-12 absolute" style="left: 463px; top: 897px;">
+        <div class="flex items-center justify-center flex-1 h-full py-3 px-16 bg-green-700 border rounded-lg border-green-700">
+            <p class="text-lg font-medium leading-relaxed text-right text-white">Pay in P2P</p>
+        </div>
+    </button>
+    <button class="w-56 h-12 absolute" style="left: 922px; top: 897px;">
+        <div class="flex items-center justify-center flex-1 h-full py-3 px-16 bg-green-700 border rounded-lg border-green-700">
+            <p class="text-lg font-medium leading-relaxed text-right text-white">Pay in USD</p>
+        </div>
+    </button>
+    <img class="absolute rounded-2xl" style="width: 876px; height: 203px; left: 402px; top: 300px;" src="https://via.placeholder.com/876x203"/>
+</div>
+
 <Navbar/>
