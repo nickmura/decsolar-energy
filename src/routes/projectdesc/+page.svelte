a36dca8c43b923c23795f032803fa17b96f0fb89
patrickbdevaney <79435941+patrickbdevaney@users.noreply.github.com>
2023-03-04 23:11:00 -0700

    finished project description page

--- src/routes/projectdesc/+page.svelte
+++ src/routes/projectdesc/+page.svelte
@@ -69,7 +69,7 @@ import Navbar from '$lib/reusable/Navbar.svelte'
                 <p class="text-lg font-semibold text-green-700">14.7%</p>
             </div>
         </div>
-        <p class="text-base font-medium leading-normal text-gray-800">Protected Earnings </p>
+        <p class="text-base font-medium leading-normal text-gray-800">Projected Earnings </p>
         <div class="relative" style="width: 674px; height: 289px;">
             <div class="inline-flex space-x-5 items-end justify-end absolute right-0 bottom-0" style="width: 595px; height: 245px;">
                 <div class="w-5 h-48 bg-green-700 rounded-3xl"/>
@@ -88,15 +88,8 @@ import Navbar from '$lib/reusable/Navbar.svelte'
                 <div class="w-5 h-56 bg-green-700 rounded-3xl"/>
                 <div class="w-5 h-full bg-green-700 rounded-3xl"/>
             </div>
-            <div class="inline-flex flex-col space-y-7 items-start justify-end w-16 h-64 absolute" style="left: 387px; top: 731px;">
-                <p class="text-base text-gray-500">$20000</p>
-                <p class="text-base text-gray-500">$10000</p>
-                <p class="text-base text-gray-500">$5000</p>
-                <p class="text-base text-gray-500">$1000</p>
-                <p class="text-base text-gray-500">$500</p>
-                <p class="text-base text-gray-500">$0</p>
-            </div>
-            <div class="inline-flex space-x-7 items-center justify-end absolute right-0 bottom-0" style="width: 585px; height: 21px;">
+          
+            <div class="inline-flex space-x-7 items-center justify-end absolute right-0 -bottom-5" style="width: 585px; height: 21px;">
                 <p class="text-base text-gray-500">1</p>
                 <p class="text-base text-gray-500">2</p>
                 <p class="text-base text-gray-500">3</p>
@@ -114,8 +107,8 @@ import Navbar from '$lib/reusable/Navbar.svelte'
                 <p class="text-base text-gray-500">15</p>
             </div>
         </div>
-        <div class="w-1/3 h-12">
-            <div class="flex items-center justify-center flex-1 h-full py-3 px-16 border rounded-lg border-green-700">
+        <div class="w-1/3= h-12">
+            <div class="flex items-center justify-center flex-1 h-full py-3 px-16 border rounded-lg border-green-700 relative -right-40 ">
                 <p class="text-lg font-medium leading-relaxed text-right text-green-700">Download</p>
             </div>
         </div>
@@ -140,48 +133,56 @@ import Navbar from '$lib/reusable/Navbar.svelte'
                 <p class="text-lg font-bold text-gray-800">Contribution Progress </p>
                 <p class="text-base font-semibold leading-normal text-center text-gray-500" style="width: 435px;">Cost of The Project               <div> $500,000 USD</div> 
             </div>
-            <div class="flex flex-col space-y-4 items-center justify-center w-90 h-10">
+            <div class="flex flex-col space-y-1 items-center justify-center w-90 h-10">
                 <p class="text-lg font-extrabold text-green-700 relative -right-20">14.7%  Financed</p>
             </div>
             <div class="relative" style="width: 404px; height: 136px;">
                 <p class="relative left-20 bottom-30 text-lg font-extralight text-gray-800">Project Road Map</p>
-                <p class="absolute right-0 bottom-0 text-base text-gray-500">Start of installation</p>
-                <p class="absolute right-0 bottom-0 text-base text-gray-500">Estimated start of operation:</p>
-                <p class="absolute right-0 bottom-0 text-base text-gray-500">First estimated profitability:</p>
-                <p class="absolute right-0 bottom-0 text-lg font-semibold text-right text-green-700">22/02/23</p>
-                <p class="absolute right-0 bottom-0 text-lg font-semibold text-right text-green-700">11/08/23</p>
-                <p class="absolute right-0 bottom-0 text-lg font-semibold text-right text-green-700">15/09/23</p>
+                <p class="absolute left-0 bottom-30 text-base text-gray-500">Start of installation:</p>
+                <p class="relative left-0 -bottom-7 text-base text-gray-500">Estimated start of operation:</p>
+                <p class="relative left-0 -bottom-7 text-base text-gray-500">First estimated profitability:</p>
+                <p class="absolute right-0 top-11 text-base font-semibold text-right text-green-700">22/02/23</p>
+                <p class="relative right-0 -top-7 text-base font-semibold text-right text-green-700">11/08/23</p>
+                <p class="relative right-0 -top-8 text-base font-semibold text-right text-green-700">15/09/23</p>
             </div>
             <div class="w-1/2 h-12">
                 <div class="flex items-center justify-center flex-1 h-full py-3 px-16 bg-green-700 border rounded-lg border-green-700 relative -right-20">
-                    <p class="text-lg font-medium leading-relaxed text-right text-white ">Participate</p>
+                    <button class="text-lg font-medium leading-relaxed text-right text-white ">Participate</button>
                 </div>
             </div>
         </div>
     </div>
-    <div class="inline-flex flex-col space-y-4 items-center justify-start px-5 pt-4 pb-5 absolute bg-white shadow rounded-2xl" style="width: 478px; height: 501px; left: 1153px; top: 731px;">
+    <div class="inline-flex flex-col space-y-1 items-center justify-start px-5 pt-4 pb-5 absolute bg-white shadow rounded-2xl" style="width: 478px; height: 501px; left: 1153px; top: 731px;">
         <p class="text-lg font-bold text-gray-800">Estimate Your Profits </p>
         <p class="text-lg font-extralight text-gray-800">400,206</p>
         <p class="text-lg font-extralight text-green-700">$200,103</p>
         <p class="w-9 text-lg font-bold text-green-700">P2P</p>
-        <img class="w-6 h-6" src="https://via.placeholder.com/26x24"/>
-        <p class="text-base leading-normal text-center text-white" style="width: 435px;">The Minimum Investment is 10 USD </p>
+      
+       
         <div class="relative" style="width: 438px; height: 138px;">
+
             <p class="absolute right-0 bottom-0 text-lg font-extralight text-gray-800">At the end of the project you will have obtained approx:</p>
-            <p class="absolute right-0 bottom-0 text-base text-gray-500">Net Profit</p>
-            <p class="absolute right-0 bottom-0 text-base text-gray-500">Bonus Profit</p>
-            <p class="absolute right-0 bottom-0 text-base text-gray-500">Total Profit</p>
+
             <p class="absolute right-0 bottom-0 text-lg font-semibold text-right text-green-700">$938,613.18</p>
-            <p class="absolute right-0 bottom-0 text-lg font-semibold text-right text-green-700">$0,000</p>
-            <p class="absolute right-0 bottom-0 text-lg font-semibold text-right text-green-700">$938,613.18</p>
+
         </div>
         <div class="w-1/2 h-12">
-            <div class="flex items-center justify-center flex-1 h-full py-3 px-16 bg-green-700 border rounded-lg border-green-700">
-                <p class="text-lg font-medium leading-relaxed text-right text-white">Participate</p>
+            <div class="flex items-center justify-center flex-1 h-full py-3 px-16 bg-green-700 border rounded-lg border-green-700 relative -bottom-10">
+                <button class="text-lg font-medium leading-relaxed text-right text-white">Participate</button>
             </div>
         </div>
     </div>
-    <div class="absolute bg-green-700" style="width: 479px; height: 93px; left: 1152px; top: 888px;"/>
+
+    <div class="inline-flex flex-col space-y-5 items-start justify-end w-16 h-64 relative left-20 bottom-30" style="left: 420px; top: 900px;">
+        <p class="text-base text-gray-500">$20000</p>
+        <p class="text-base text-gray-500">$10000</p>
+        <p class="text-base text-gray-500">$5000</p>
+        <p class="text-base text-gray-500">$1000</p>
+        <p class="text-base text-gray-500">$500</p>
+        <p class="text-base text-gray-500">$0</p>
+    </div>
+   
     <img class="w-5 h-5 absolute" style="left: 1253px; top: 214px;"  src="../../../img/p2pcoin.png" alt="P2P Token"/>
+    <img class="w-5 h-5 absolute" style="left: 1315px; top: 805px;"  src="../../../img/p2pcoin.png" alt="P2P Token"/>
     <img class="absolute rounded-2xl" style="width: 693px; height: 255px; left: 416px; top: 235px;" src="../../../../img/solarBaranquilla.jpg" alt="solarBaranquilla"/>
 </div>
