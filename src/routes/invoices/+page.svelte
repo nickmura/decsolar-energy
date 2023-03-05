84c5994bd7af1a7dae3e4f2ad6ac8cb26d933237
patrickbdevaney <79435941+patrickbdevaney@users.noreply.github.com>
2023-03-04 20:36:49 -0700

    styled invoice page more

--- src/routes/invoices/+page.svelte
+++ src/routes/invoices/+page.svelte
@@ -4,29 +4,12 @@
     
 
 </script>
+<Navbar/>
 
 
-<div>
-    <div class="absolute" style="width: 876px; height: 702px; left: 402px; top: 299px;">
-        <div class="inline-flex flex-col space-y-7 items-start justify-start pb-72 bg-white shadow rounded-2xl" style="width: 876px; height: 702px;">
-            <img class="rounded-2xl" style="width: 876px; height: 203px;" src="../../../../img/solarBaranquilla.jpg" alt="Solar Baranquilla"/>
-            <div class="flex flex-col items-start justify-start" style="width: 783px; height: 166.65px;">
-                <p class="text-2xl font-bold text-gray-800" style="width: 424.91px; height: 62.65px;">Hospital Of Baranquilla</p>
-                <p class="w-72 h-12 text-lg text-gray-500">Baranquilla, Colombia</p>
-                <p class="text-2xl font-bold text-gray-800" style="width: 424.91px; height: 62.65px;">Total Usage Month Kwh</p>
-            </div>
-        </div>
-    </div>
-    <div class="absolute" style="width: 876px; height: 702px; left: 402px; top: 299px;">
-        <div class="inline-flex flex-col space-y-7 items-start justify-start pb-72 bg-white shadow rounded-2xl" style="width: 876px; height: 702px;">
-            <img class="rounded-2xl" style="width: 876px; height: 203px;" src="../../../../img/solarBaranquilla.jpg" alt=""/>
-            <div class="flex flex-col items-start justify-start" style="width: 783px; height: 166.65px;">
-                <p class="text-2xl font-bold text-gray-800" style="width: 424.91px; height: 62.65px;">Hospital Of Baranquilla</p>
-                <p class="w-72 h-12 text-lg text-gray-500">Baranquilla, Colombia</p>
-                <p class="text-2xl font-bold text-gray-800" style="width: 424.91px; height: 62.65px;">Total Usage Month Kwh</p>
-            </div>
-        </div>
-    </div>
+    
+    <div class='flex justify-center relative bg-gray-100' style="width: 1607px; height: 1040px;"></div>
+    <p class="fixed left-80 top-40 text-7xl font-bold text-gray-800" style="width: 800.91px; height: 62.65px;">Pay Bill</p>
     <div class="absolute" style="width: 876px; height: 702px; left: 402px; top: 299px;">
         <div class="flex items-end justify-start pl-6 pr-16 pt-56 pb-36 bg-white shadow rounded-2xl" style="width: 876px; height: 702px;">
             <div class="relative" style="width: 783px; height: 322.65px;">
@@ -52,6 +35,5 @@
         </div>
     </button>
     <img class="absolute rounded-2xl" style="width: 876px; height: 203px; left: 402px; top: 300px;" src="../../../../img/solarBaranquilla.jpg" alt=""/>
-</div>
 
-<Navbar/>
+
